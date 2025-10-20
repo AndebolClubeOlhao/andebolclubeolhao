841a7938ae7a55896639eb9f8332ca11c0af6c8f
Leonardo Brito <brito10055@gmail.com>
2025-10-20 15:49:46 +0100

    loja

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import express from "express";
+
 import path from "path";
 import { fileURLToPath } from "url";
 import nodemailer from "nodemailer";
@@ -11,7 +11,7 @@ dotenv.config();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const app = express();
+
 
 // Servir arquivos estáticos da pasta "public"
 app.use(express.static(path.join(__dirname)));
